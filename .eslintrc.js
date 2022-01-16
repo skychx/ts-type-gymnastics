c634e9f56dab38c9e4f60fdf744a7e84dae33329
skychx <skychx@hotmail.com>
2022-01-16 16:34:01 +0800

    style: format config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
-    extends: [
-        'eslint-config-airbnb-base',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended'
-    ],
+    extends: ['eslint-config-airbnb-base', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     rules: {
@@ -12,11 +8,13 @@ module.exports = {
         '@typescript-eslint/no-use-before-define': 'off',
         'no-prototype-builtins': 'off',
         'import/prefer-default-export': 'off',
-        '@typescript-eslint/no-unused-vars': ["warn", 
-            { 
-                "varsIgnorePattern": "cases|test|_|R|error", 
-                "args": "none",
-            }],
+        '@typescript-eslint/no-unused-vars': [
+            'warn',
+            {
+                varsIgnorePattern: 'cases|test|_|R|error',
+                args: 'none',
+            },
+        ],
         '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/no-empty-function': 'off',
         'import/no-unresolved': 'off',
@@ -25,8 +23,8 @@ module.exports = {
         'import/resolver': {
             node: {
                 extensions: ['.js', '.ts'],
-                moduleDirectory: ['node_modules', './src']
-            }
+                moduleDirectory: ['node_modules', './src'],
+            },
         },
     },
     parserOptions: {
--- README.md
+++ README.md
@@ -15,6 +15,3 @@ src
     ├── hard
     └── medium
 ```
-
-
-
