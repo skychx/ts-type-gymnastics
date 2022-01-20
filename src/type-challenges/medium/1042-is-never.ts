5b9ea7e706e5adc9e36d32f3b658c967365a0801
skychx <skychx@hotmail.com>
2022-01-20 23:33:32 +0800

    feat: add isNever & isUnion

---
+++ src/type-challenges/medium/1042-is-never.ts
@@ -0,0 +1,16 @@
+import { Equal, Expect } from '@type-challenges/utils';
+
+// answer
+type IsNever<T> = [T] extends [never] ? true : false;
+
+// test cases
+// @ts-ignore
+type cases = [
+    Expect<Equal<IsNever<never>, true>>,
+    Expect<Equal<IsNever<never | string>, false>>,
+    Expect<Equal<IsNever<''>, false>>,
+    Expect<Equal<IsNever<undefined>, false>>,
+    Expect<Equal<IsNever<null>, false>>,
+    Expect<Equal<IsNever<[]>, false>>,
+    Expect<Equal<IsNever<{}>, false>>,
+];
---
+++ src/type-challenges/medium/1097-is-union.ts
@@ -0,0 +1,24 @@
+import { Equal, Expect } from '@type-challenges/utils';
+
+/**
+ * answer
+ * @url https://blog.csdn.net/weixin_43786756/article/details/120862671
+ */
+type IsUnion<T, U = T> = T extends U ? ([U] extends [T] ? false : true) : never;
+
+// test cases
+// @ts-ignore
+type cases = [
+    Expect<Equal<IsUnion<string>, false>>,
+    Expect<Equal<IsUnion<string | number>, true>>,
+    Expect<Equal<IsUnion<'a' | 'b' | 'c' | 'd'>, true>>,
+    Expect<Equal<IsUnion<undefined | null | void | ''>, true>>,
+    Expect<Equal<IsUnion<{ a: string } | { a: number }>, true>>,
+    Expect<Equal<IsUnion<{ a: string | number }>, false>>,
+    Expect<Equal<IsUnion<[string | number]>, false>>,
+    // Cases where T resolves to a non-union type.
+    Expect<Equal<IsUnion<string | never>, false>>,
+    Expect<Equal<IsUnion<string | unknown>, false>>,
+    Expect<Equal<IsUnion<string | any>, false>>,
+    Expect<Equal<IsUnion<string | 'a'>, false>>,
+];
